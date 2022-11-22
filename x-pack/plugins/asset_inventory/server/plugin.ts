6ac0beba236067bb6fac925aa2576a9360b9aa34
Jason Rhodes <jason.rhodes@elastic.co>
2022-11-22 12:33:08 -0500

    Asset inventory list

--- x-pack/plugins/asset_inventory/server/plugin.ts
+++ x-pack/plugins/asset_inventory/server/plugin.ts
@@ -5,8 +5,11 @@
  * 2.0.
  */
 
+import { schema } from '@kbn/config-schema';
 import { Plugin, CoreSetup } from '@kbn/core/server';
+import { AssetFilters } from '../common/types_api';
 import { getAssets } from './lib/get_assets';
+import { getValuesForField } from './lib/get_values_for_field';
 
 export type AssetInventoryServerPluginSetup = ReturnType<AssetInventoryServerPlugin['setup']>;
 
@@ -27,16 +30,45 @@ export class AssetInventoryServerPlugin implements Plugin<AssetInventoryServerPl
       }
     );
 
-    router.get(
+    router.get<unknown, AssetFilters | undefined, unknown>(
       {
         path: '/api/asset-inventory',
-        validate: false,
+        validate: {
+          query: schema.any({}),
+        },
       },
       async (context, req, res) => {
+        const filters = req.query || {};
+
         try {
-          const assets = await getAssets();
+          const assets = await getAssets({ filters });
           return res.ok({ body: { assets } });
         } catch (error: unknown) {
+          // console.log('error looking up asset records', error);
+          return res.customError({ statusCode: 500 });
+        }
+      }
+    );
+
+    router.get(
+      {
+        path: '/api/asset-inventory/field-values',
+        validate: {
+          query: schema.any({}),
+        },
+      },
+      async (context, req, res) => {
+        const { field, searchText, version } = req.query;
+
+        try {
+          const results = await getValuesForField({
+            field,
+            searchText,
+            version: Number(version),
+          });
+          return res.ok({ body: { results } });
+        } catch (error: unknown) {
+          // console.log('error looking up field values', error);
           return res.customError({ statusCode: 500 });
         }
       }
